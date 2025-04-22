7bea4364d23d83c48dfc7babd1bf3a117464c050
shahryar-abbasi <shahryarabbasii7@gmail.com>
2025-04-22 11:01:48 +0500

    Add files via upload

---
+++ class-practice.py
@@ -0,0 +1,16 @@
+class Laptop:
+    def name(self):
+        print("dell")
+    def attributes(self):
+        print("16gb rom and 512gb rom")
+    def price(self):
+        print("150k$")
+#obj=Laptop()
+#obj.name()
+#obj.attributes()
+#obj.price()
+
+
+
+
+        
